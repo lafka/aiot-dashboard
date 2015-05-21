5419fa852ffd5d120d650639e8312cf4fd81e9f4
Helge Milde <helge@monsternett.no>
2015-05-21 15:36:37 +0200

    Visualize power meters using flot

--- aiot_dashboard/apps/dashboard/urls.py
+++ aiot_dashboard/apps/dashboard/urls.py
@@ -9,10 +9,13 @@ urlpatterns = [
     # room overview
     url(r'^rooms/$', views.RoomOverviewView.as_view(), name='dashboard_room_overview'),
     url(r'^rooms/state/$', views.room_overview_state, name='dashboard_room_overview_state'),
-    # room vivew
+    # room view
     url(r'^rooms/(?P<room_id>\d+)/$', views.RoomView.as_view(), name='dashboard_room'),
     url(r'^rooms/(?P<room_id>\d+)/state/$', views.room_state, name='dashboard_room_state'),
     # power meters overview
     url(r'^power-meters/$', views.PowerMeterOverviewView.as_view(), name='dashboard_power_meter_overview'),
     url(r'^power-meters/state/$', views.power_meter_overview_state, name='dashboard_power_meter_overview_state'),
+    # power meters view
+    url(r'^power-meters/(?P<device_key>\w+)/$', views.PowerMeterView.as_view(), name='dashboard_power_meter'),
+    url(r'^power-meters/(?P<device_key>\w+)/state/$', views.power_meter_state, name='dashboard_power_meter_state'),
 ]
--- aiot_dashboard/apps/devices/models.py
+++ aiot_dashboard/apps/devices/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from collections import OrderedDict
+from datetime import datetime, timedelta
 
 from django.db import models, connection
 
@@ -56,3 +58,55 @@ class PowerMeterTimeseries(models.Model):
     class Meta:
         managed = False
         db_table = 'power_meter_timeseries'
+
+    @classmethod
+    def get_latest_kwm(cls, device):
+        last_two = cls.objects.filter(device=device).order_by('-datetime')[:2]
+
+        if last_two.count() != 2:
+            return None
+
+        last, next_to_last = last_two
+        time_diff = last.datetime - next_to_last.datetime
+
+        multiplier = 60 / time_diff.total_seconds()
+        calibration_factor = 10000.0
+        return int(last.pulses * multiplier / calibration_factor)
+
+    @classmethod
+    def get_latest_kwh(cls, device):
+        one_hour_ago = datetime.now() - timedelta(hours=1)
+        measures = list(cls.objects.filter(device=device, datetime__gt=one_hour_ago).order_by('-datetime'))
+
+        if len(measures) < 2:
+            return
+
+        total_pulses = sum(measure.pulses for measure in measures)
+        time_diff = measures[0].datetime - measures[-1].datetime
+        calibration_factor = 10000.0
+        return int((total_pulses / calibration_factor) * (3600 / time_diff.total_seconds()))
+
+    @classmethod
+    def get_kwh_timeseries(cls, device, from_datetime, to_datetime):
+        kwh_per_hour = OrderedDict()
+        datetime_it = from_datetime
+
+        while datetime_it <= to_datetime:
+            hour = datetime(datetime_it.year, datetime_it.month, datetime_it.day, datetime_it.hour)
+            next_hour = hour + timedelta(hours=1)
+
+            datetime_it = datetime_it + timedelta(hours=1)
+
+            measures = list(cls.objects.filter(device=device, datetime__gte=hour,
+                                               datetime__lt=next_hour).order_by('-datetime'))
+
+            if len(measures) < 2:
+                continue
+
+            total_pulses = sum(measure.pulses for measure in measures)
+            time_diff = measures[0].datetime - measures[-1].datetime
+            calibration_factor = 10000.0
+            kwh_per_hour[hour] = int((total_pulses / calibration_factor) * (3600 / time_diff.total_seconds()))
+
+
+        return kwh_per_hour
