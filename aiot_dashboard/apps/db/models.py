b3c242af671a742e416a82f5429c4d4566bba747
Mark Skelton <mark@software13.co.uk>
2015-06-03 20:59:29 +0100

    Updated database schema and model definitions. Ported and adjusted room sync command, now only imports rooms. Removed old devices app.

--- aiot_dashboard/apps/db/models.py
+++ aiot_dashboard/apps/db/models.py
@@ -3,6 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Deviations(models.Model):
+    datetime = models.DateTimeField(blank=True, null=True)
+    device_key = models.ForeignKey('Device', db_column='device_key', blank=True, null=True)
+    deviation_type = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'deviations'
+
+
 class Device(models.Model):
     key = models.TextField(primary_key=True)
     name = models.TextField()
@@ -20,7 +30,6 @@ class MapDevicePowerCircuit(models.Model):
     class Meta:
         managed = False
         db_table = 'map_device_power_circuit'
-        unique_together = (('device_key', 'power_circuit'),)
 
 
 class MapDeviceRoom(models.Model):
@@ -44,11 +53,20 @@ class PowerCircuit(models.Model):
 class Room(models.Model):
     key = models.TextField(primary_key=True)
     name = models.TextField()
-    type = models.TextField()
+    room_type = models.ForeignKey('RoomType', blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'room'
+
+
+class RoomType(models.Model):
+    description = models.TextField()
+    manminutes_capacity = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'room_type'
 
 
 class TsCo2(models.Model):
@@ -71,6 +89,26 @@ class TsDecibel(models.Model):
     class Meta:
         managed = False
         db_table = 'ts_decibel'
+
+
+class TsEnergyProductivity(models.Model):
+    datetime = models.DateTimeField(blank=True, null=True)
+    device_key = models.ForeignKey(Device, db_column='device_key', blank=True, null=True)
+    value = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'ts_energy_productivity'
+
+
+class TsKwh(models.Model):
+    datetime = models.DateTimeField(blank=True, null=True)
+    device_key = models.ForeignKey(Device, db_column='device_key')
+    value = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'ts_kwh'
 
 
 class TsKwm(models.Model):
@@ -116,10 +154,20 @@ class TsMovement(models.Model):
         db_table = 'ts_movement'
 
 
+class TsPersonsInside(models.Model):
+    datetime = models.DateTimeField(blank=True, null=True)
+    device_key = models.ForeignKey(Device, db_column='device_key', blank=True, null=True)
+    value = models.FloatField()
+
+    class Meta:
+        managed = False
+        db_table = 'ts_persons_inside'
+
+
 class TsPulses(models.Model):
     datetime = models.DateTimeField(blank=True, null=True)
     device_key = models.ForeignKey(Device, db_column='device_key')
-    value = models.FloatField()
+    value = models.IntegerField()
     packet_number = models.IntegerField()
 
     class Meta:
--- conf/vagrant/schema.sql
+++ conf/vagrant/schema.sql
@@ -1,8 +1,14 @@
 -- Main entities
+CREATE TABLE room_type (
+    id SERIAL PRIMARY KEY,
+    description TEXT NOT NULL,
+    manminutes_capacity INTEGER NOT NULL
+);
+
 CREATE TABLE room (
     key TEXT PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
-    type TEXT NOT NULL
+    room_type_id INTEGER REFERENCES room_type(id)
 );
 
 CREATE TABLE device (
@@ -21,13 +27,19 @@ CREATE TABLE map_device_room (
     device_key TEXT REFERENCES device (key) NOT NULL,
     room_key TEXT REFERENCES room (key) NOT NULL
 );
-CREATE UNIQUE INDEX map_device_room_keys_unique_idx ON map_device_room (device_key, room_key);
+CREATE UNIQUE INDEX
+    map_device_room_keys_unique_idx
+ON
+    map_device_room (device_key, room_key);
 
 CREATE TABLE map_device_power_circuit (
     device_key TEXT REFERENCES device (key) NOT NULL,
     power_circuit_id INTEGER REFERENCES power_circuit (id) NOT NULL
 );
-CREATE UNIQUE INDEX map_device_power_circuit_keys_unique_idx ON map_device_power_circuit (device_key, power_circuit_id);
+CREATE UNIQUE INDEX
+    map_device_power_circuit_keys_unique_idx
+ON
+    map_device_power_circuit (device_key, power_circuit_id);
 
 -- "Raw" time series
 CREATE TABLE ts_co2 (
@@ -47,7 +59,7 @@ CREATE TABLE ts_decibel (
 CREATE TABLE ts_movement (
     datetime TIMESTAMP WITH TIME ZONE,
     device_key TEXT REFERENCES device (key) NOT NULL,
-    value boolean NOT NULL,
+    value BOOLEAN NOT NULL,
     packet_number INTEGER NOT NULL
 );
 
@@ -75,13 +87,47 @@ CREATE TABLE ts_light (
 CREATE TABLE ts_pulses (
     datetime TIMESTAMP WITH TIME ZONE,
     device_key TEXT REFERENCES device (key) NOT NULL,
-    value DOUBLE PRECISION NOT NULL,
+    value INTEGER NOT NULL,
     packet_number INTEGER NOT NULL
 );
 
--- Other time series
+-- Other time series (that is calculated based on sensor data "in the
+-- background")
+
+-- Calculated from `ts_pulses`
 CREATE TABLE ts_kwm (
     datetime TIMESTAMP WITH TIME ZONE,
     device_key TEXT REFERENCES device (key) NOT NULL,
     value DOUBLE PRECISION NOT NULL
+);
+
+-- Calculated from `ts_kwm`
+CREATE TABLE ts_kwh (
+    datetime TIMESTAMP WITH TIME ZONE,
+    device_key TEXT REFERENCES device (key) NOT NULL,
+    value DOUBLE PRECISION NOT NULL
+);
+
+-- Calculated from sensor data
+CREATE TABLE ts_persons_inside (
+    id SERIAL PRIMARY KEY,
+    datetime TIMESTAMP WITH TIME ZONE,
+    device_key TEXT REFERENCES device(key),
+    value DOUBLE PRECISION NOT NULL
+);
+
+-- Calculated from `ts_persons_inside` and `ts_kwm`.
+CREATE TABLE ts_energy_productivity (
+    id SERIAL PRIMARY KEY,
+    datetime TIMESTAMP WITH TIME ZONE,
+    device_key TEXT REFERENCES device(key),
+    value DOUBLE PRECISION NOT NULL
+);
+
+-- Deviations
+CREATE TABLE deviations (
+    id SERIAL PRIMARY KEY,
+    datetime TIMESTAMP WITH TIME ZONE,
+    device_key TEXT REFERENCES device(key),
+    deviation_type TEXT NOT NULL
 );
