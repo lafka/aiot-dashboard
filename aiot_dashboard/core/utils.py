04ac79903a074ac02bb5d878c113170606144d73
Helge Milde <helge@monsternett.no>
2015-06-12 11:26:03 +0200

    Use `timezone.localtime` to plot according to NO

--- aiot_dashboard/core/utils.py
+++ aiot_dashboard/core/utils.py
@@ -5,11 +5,12 @@ from django.utils import timezone
 
 
 def get_today():
-    now = timezone.now()
+    now = timezone.localtime(timezone.now())
     return datetime.datetime(now.year, now.month, now.day).replace(tzinfo=now.tzinfo)
 
 def to_epoch_mili(dt):
     """Converts Python `datetime` object to an UTC epoch in miliseconds (to be
     used with `flot`)
     """
+    dt = timezone.localtime(dt)
     return calendar.timegm(dt.timetuple()) * 1000
